32ca95b8b6ef38403aec5dcda7ae0ae7af63c15f
orielbachar <oriel.bachar@gmail.com>
2017-01-25 22:02:33 +0200

    fb

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,7 @@ var router = express.Router();
 
 var User = require('../models/Users.js')
 
-//register
-router.post('/', function(req, res, next) {
-  var user = new User(req.body);
-  user.save(function(err,user){
-    if(err){return next (err)};
-    res.send(user);
-  });
-});
+
 
 router.use(expressSession({ secret: 'mySecretKey' }));
 
@@ -23,7 +16,7 @@ router.use(passport.session());
 
 //auth with facebook strategy
 router.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html' + req.user);
 });
 
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email']}));
@@ -32,17 +25,19 @@ passport.use(new FacebookStrategy({
     clientID: '1771236643200897',
     clientSecret: 'c68ee6532ac101fa8cc3344f7b3b51e4',
     callbackURL: "http://localhost:8000/auth/facebook/callback",
-    profileFields: ['id', 'email', 'displayName', 'photos']
+    profileFields: ['id', 'email', 'displayName', 'photos'],
+    passReqToCallback:true
+
   },
   function(accessToken, refreshToken, profile, done) {
-    // console.log("accessToken:");
-    // console.log(accessToken);
+    console.log("accessToken:");
+    console.log(accessToken);
 
-    // console.log("refreshToken:");
-    // console.log(refreshToken);
+    console.log("refreshToken:");
+    console.log(refreshToken);
 
-    // console.log("profile:");
-    // console.log(profile);
+    console.log("profile:");
+    console.log(profile);
 
     return done(null, profile);
   }
@@ -50,10 +45,9 @@ passport.use(new FacebookStrategy({
 
 router.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
-    successRedirect : '/profile',
+    successRedirect : '/',
     failureRedirect : '/facebookCanceled'
 }));
-
 
 
 // route for showing the profile page
